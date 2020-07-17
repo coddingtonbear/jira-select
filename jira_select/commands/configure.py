115a5c6894d8e4134f99e062c1f1287549b4fae6
Adam Coddington <me@adamcoddington.net>
2020-07-16 21:21:38 -0700

    Swapping pyinquirer for prompt-toolkit in 'configuration' command.

--- jira_select/commands/configure.py
+++ jira_select/commands/configure.py
@@ -1,31 +1,15 @@
 from logging import getLogger
-from typing import Optional
-from urllib.parse import urlparse
+from typing import Tuple
 
 import keyring
 from jira import JIRA, JIRAError
+from prompt_toolkit.shortcuts import input_dialog, yes_no_dialog
 
 from ..constants import APP_NAME
 from ..exceptions import UserError
 from ..plugin import BaseCommand
 
 logger = getLogger(__name__)
-
-
-def validate_url(url: Optional[str]) -> bool:
-    if url is None:
-        return False
-
-    parsed = urlparse(url)
-
-    return bool(parsed.scheme and parsed.netloc)
-
-
-def require(data: Optional[str]) -> bool:
-    if not data:
-        return False
-
-    return True
 
 
 class Command(BaseCommand):
@@ -36,45 +20,55 @@ class Command(BaseCommand):
             "to your Jira instance."
         )
 
+    def collect_credentials(self) -> Tuple[str, str, str]:
+        instance_url = input_dialog(
+            title="Instance URL",
+            text="Jira instance URL (example: 'https://mycompany.jira.com/')",
+        ).run()
+        import pdb
+
+        pdb.set_trace()
+        username = input_dialog(title="Username", text="Username").run()
+        password = input_dialog(
+            title="Password", text=f"Password for {username}: ", password=True
+        ).run()
+
+        return instance_url, username, password
+
     def handle(self) -> None:
-        instance_url = self.prompt(
-            {
-                "type": "input",
-                "message": "Jira instance URL (example: 'https://mycompany.jira.com/')",
-                "validate": validate_url,
-            }
-        )
-        username = self.prompt(
-            {"type": "input", "message": "Username", "validate": require}
-        )
-        password = self.prompt(
-            {
-                "type": "password",
-                "message": f"Password for {username}",
-                "validate": require,
-            }
-        )
+        instance_url = ""
+        username = ""
+        password = ""
 
-        logger.info("Connected to Jira successfully; configuration updated.")
+        while True:
+            instance_url, username, password = self.collect_credentials()
 
-        try:
-            JIRA(
-                instance_url,
-                options={"agile_rest_path": "agile",},
-                basic_auth=(username, password),
-                max_retries=0,
-            )
-        except JIRAError:
-            raise UserError("Could not connect to Jira using the provided credentials")
+            try:
+                JIRA(
+                    instance_url,
+                    options={"agile_rest_path": "agile",},
+                    basic_auth=(username, password),
+                    max_retries=0,
+                )
+                break
+            except JIRAError:
+                result = yes_no_dialog(
+                    title="Error connecting to Jira",
+                    text=(
+                        "A connection to Jira could not be established using "
+                        "the credentials you provided; try again?"
+                    ),
+                ).run()
+                if not result:
+                    raise UserError("Aborted; configuration not saved.")
 
         self.config.update({"instance_url": instance_url, "username": username})
 
-        logger.info("Connected to Jira successfully; configuration updated.")
-
         self.save_config()
 
-        store_password = self.prompt(
-            {"type": "confirm", "message": "Store password in keyring?"}
-        )
+        store_password = yes_no_dialog(
+            title="Save password?",
+            text="Would you like to save this password to your system keyring?",
+        ).run()
         if store_password:
             keyring.set_password(APP_NAME, instance_url + username, password)
