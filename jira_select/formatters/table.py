19c212de7dc6cc16e742d932eaa45912db69a754
Adam Coddington <me@adamcoddington.net>
2020-07-19 12:22:12 -0700

    Adding new 'table' formatter.

---
+++ jira_select/formatters/table.py
@@ -0,0 +1,36 @@
+from typing import Any, Dict, List
+
+from rich.console import Console
+from rich.table import Table
+
+from ..plugin import BaseFormatter
+
+
+class Formatter(BaseFormatter):
+    @classmethod
+    def get_file_extension(cls) -> str:
+        return "csv"
+
+    def _generate_fieldnames(self) -> List[str]:
+        fields = []
+
+        for field in self.query.get_fields():
+            fields.append(field["column"])
+
+        return fields
+
+    def open(self):
+        super().open()
+        self.table = Table()
+
+        for field in self.query.get_fields():
+            self.table.add_column(field["column"])
+
+    def close(self):
+        console = Console()
+        console.print(self.table)
+        super().close()
+
+    def writerow(self, row: Dict[str, Any]):
+        fields = [str(row.get(field["column"])) for field in self.query.get_fields()]
+        self.table.add_row(*fields)
