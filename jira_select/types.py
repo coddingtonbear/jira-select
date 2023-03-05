6cd23298c71bd1cef90ef3953ccb2458eb5c9160
Adam Coddington <adam.coddington@airship.com>
2023-03-04 18:43:52 -0800

    Updating model definition for QueryDefinition.

--- jira_select/types.py
+++ jira_select/types.py
@@ -44,6 +44,9 @@ class QueryDefinition(BaseModel):
     cap: Optional[int]
     cache: Optional[Union[int, Tuple[Optional[int], Optional[int]]]]
 
+    class Config:
+        allow_population_by_field_name = True
+
 
 class SchemaRow(BaseModel):
     id: str
