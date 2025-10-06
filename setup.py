2205c4f618d7346b0a9f719205d584ab72527987
Adam Coddington <adam.coddington@airship.com>
2025-10-06 18:25:51 -0500

    Bump version: 3.3.1 → 3.4.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(*names, **kwargs):
 
 setup(
     name="jira-select",
-    version="3.3.1",
+    version="3.4.0",
     license="MIT",
     description=(
         "Easily run complex SQL-like queries far beyond what "
